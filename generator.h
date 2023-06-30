3cbf2932242696d0104efb2b86726bed30efd7c5
zsw <2013484@nankai.edu.cn>
2023-06-30 08:30:54 +0800

    h

--- generator.h
+++ generator.h
@@ -11,10 +11,7 @@
 using namespace std;
 
 #define SUDOKUPATH  "sudoku.txt"
-#define QUESPATH "ques.txt"
-#define SOLUTIONPATH "solution.txt"
-#define BASEPATH "sudokubase.txt"
-#define GENERATEPATH "generation.txt" 
+#define GENERATEPATH "game.txt" 
 #define MAX 200000000
 static char AbsolutePath[100] = { 0 };
 static char ques_board[10][20];
